c5fd0a307542a899b2fe42d89e963ebd14ac6f08
EntranceJew <EntranceJew@gmail.com>
2025-08-17 13:44:25 -0500

    make the hardware-configuration less volatile

--- misc/storage.nix
+++ misc/storage.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 
+let
+  automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  syno_secret = "/etc/nixos/secrets/smb_synology";
+  kunlan_secret = "/etc/nixos/secrets/smb_kunlan";
+in
 {
   boot.supportedFilesystems = [ "ntfs" ];
 
@@ -16,4 +21,58 @@
     grsync
     dupeguru
   ];
+
+  # filesystems
+  fileSystems."/mnt/mintman" = {
+    device = "//hbdltnasdaq.local/mintman";
+    fsType = "cifs";
+    options = [ "${automount_opts},credentials=${syno_secret},rw,uid=1000" ];
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-partuuid/aa3aeb96-8c53-49d0-b6f6-98a598390f99";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/B44A-9200";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
+
+  fileSystems."/mnt/Share" = {
+    device = "//hbdltnasdaq.local/Share";
+    fsType = "cifs";
+    options = [ "${automount_opts},credentials=${syno_secret},rw,uid=1000" ];
+  };
+
+  fileSystems."/mnt/Zumgo" = {
+    device = "//hbdltnasdaq.local/Zumgo";
+    fsType = "cifs";
+    options = [ "${automount_opts},credentials=${syno_secret},rw,uid=1000" ];
+  };
+
+  fileSystems."/mnt/PD" = {
+    device = "//nas-kunlan.local/Personal-Drive";
+    fsType = "cifs";
+    options = [ "${automount_opts},credentials=${kunlan_secret},rw,uid=1000" ];
+  };
+
+  # fileSystems."/mnt/UNAS" =
+  #   { device = "//nas-kunlan.lan/";
+  #     fsType = "cifs";
+  #     options = ["${automount_opts},credentials=${kunlan_secret},rw,uid=1000"];
+  #   };
+
+  fileSystems."/mnt/windows" = {
+    device = "/dev/disk/by-uuid/01DB8F2FB709A8A0";
+    fsType = "ntfs-3g";
+    options = [ "rw" "uid=1000" ];
+  };
+
+  fileSystems."/mnt/dmz" = {
+    device = "/dev/disk/by-uuid/413FC4F423B49867";
+    fsType = "ntfs-3g";
+    options = [ "rw" "uid=1000" ];
+  };
 }
