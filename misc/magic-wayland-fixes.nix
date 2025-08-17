e31af154eba07747e04e1f46cb6a008fc7dd25b9
EntranceJew <EntranceJew@gmail.com>
2025-08-16 23:52:36 -0500

    fix clipboard stuff?

--- misc/magic-wayland-fixes.nix
+++ misc/magic-wayland-fixes.nix
@@ -5,9 +5,9 @@
     NIXOS_OZONE_WL = "1";
   };
   environment.variables = {
-    QT_QPA_PLATFORM = "wayland-egl;xcb";
-    MOZ_ENABLE_WAYLAND = "1";
+    # QT_QPA_PLATFORM = "wayland-egl;xcb";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    MOZ_ENABLE_WAYLAND = "1";
     ENCORE_EVAS_ENGINE = "wayland_egl";
     ELM_ENGINE = "wayland_egl";
     _JAVA_AWT_WM_NONREPARENTING = "1";
