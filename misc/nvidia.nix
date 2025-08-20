39a82dab8577c25041dc930a574b4a754edd4eb2
EntranceJew <EntranceJew@gmail.com>
2025-08-19 20:40:37 -0500

    nvidia fixes

--- misc/nvidia.nix
+++ misc/nvidia.nix
@@ -1,63 +1,37 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  nixpkgs.config.cudaSupport = true;
-  hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = true;
-  nixpkgs.config.allowUnfree = true;
+  hardware.nvidia.open = false;
+  nixpkgs.config = {
+    cudaSupport = true;
+    allowUnfree = true;
+    # nvidia.acceptLicense = true;
+  };
+  hardware.graphics.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production.overrideAttrs {
+    version = "580.76.05";
+    src = pkgs.fetchurl {
+      url = "https://us.download.nvidia.com/XFree86/Linux-x86_64/580.76.05/NVIDIA-Linux-x86_64-580.76.05.run";
+      sha256 = "1zcpbp859h5whym0r54a3xrkqdl7z3py1hg8n8hv0c89nqvfd6r1";
+    };
+  };
   # services.xserver.videoDrivers = [ "nvidia" ];
-
-  # users.users.ej.packages = with pkgs; [
-  #   libva-utils
-  # ];
-
-  # hardware.graphics.extraPackages = [
+  # hardware.nvidia.open = true;  # see the note above
+  # hardware.graphics.extraPackages = with pkgs; [
   #   pkgs.nvidia-vaapi-driver
+  #   pkgs.libva-utils
   # ];
-
-  # environment.variables = {
-  #   NVD_BACKEND = "direct";
-  #   GBM_BACKEND = "nvidia-drm";
-  #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  #   WLR_NO_HARDWARE_CURSORS = "1";
-
-  #   # NVD_BACKEND = "direct";
-  #   LIBVA_DRIVER_NAME = "nvidia";
-  # };
-  # # Other
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # boot = {
-  #   initrd.kernelModules = [ "nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
-  #   kernelParams = [ "nvidia-drm.fbdev=1" ];
-  # };
-  # nixpkgs.config = {
-  #   allowUnfree = true;
-  #   nvidia.acceptLicense = true;
-  # };
-
-  # hardware.nvidia = {
-  #   modesetting.enable = true;
-  #   powerManagement.enable = false;
-  #   powerManagement.finegrained = false;
-
-  #   open = true;
-
-  #   nvidiaSettings = true;
-
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-  #   prime = {
-  #     nvidiaBusId = "PCI:1:0:0";
-  #   };
-  # };
-
-  # hardware.graphics = {
-  #   enable = true;
-  #   enable32Bit = true;
-
-  #   # extraPackages = with pkgs; [
-  #   #   # nvidia-vaapi-driver
-  #   # ];
-  # };
+  
+  environment.variables = {
+    MOZ_DISABLE_RDD_SANDBOX = "1";
+    LIBVA_DRIVER_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+    EGL_PLATFORM = "wayland";
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+  # environment.systemPackages = with pkgs; [
+  # ];
 }
