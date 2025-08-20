f3f0eb0f8b5bb9cd860548d9c1ae886c83310b68
EntranceJew <EntranceJew@gmail.com>
2025-08-19 20:39:42 -0500

    tweaks for browser, future notes for GL threads issue

--- home.nix
+++ home.nix
@@ -7,13 +7,54 @@ in
     (import "${home-manager}/nixos")
   ];
 
+  home-manager.backupFileExtension="backup";
+
   home-manager.users.ej = { pkgs, ... }: {
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "librewolf.desktop";
+        "x-scheme-handler/http" = "librewolf.desktop";
+        "x-scheme-handler/https" = "librewolf.desktop";
+        "x-scheme-handler/about" = "librewolf.desktop";
+        "x-scheme-handler/unknown" = "librewolf.desktop";
+      };
+    };
+#     home.file = {
+#     ".nv/nvidia-application-profiles-rc".text = ''
+# {
+#     "rules": [
+#         {
+#             "pattern": {
+#                 "feature": "dso",
+#                 "matches": "libGL.so.1"
+#             },
+#             "profile": "openGL_fix"
+#         }
+#     ],
+#     "profiles": [
+#         {
+#             "name": "openGL_fix",
+#             "settings": [
+#                 {
+#                     "key": "GLThreadedOptimizations",
+#                     "value": false
+#                 }
+#             ]
+#         }
+#     ]
+# }
+#     '';
+#   };
+
+    programs.home-manager = {
+      enable = true;
+    };
+
     home.username = "ej";
     home.homeDirectory = "/home/ej";
 
     home.stateVersion = "24.11";
-
-    programs.home-manager.enable = true;
 
     /*
     home.packages = [
@@ -23,4 +64,5 @@ in
 
     
   };
+
 }
