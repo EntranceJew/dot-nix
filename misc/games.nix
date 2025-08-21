6dbf73fc864ca40549d0677488dc99ca2faa1593
EntranceJew <EntranceJew@gmail.com>
2025-08-21 10:16:57 -0500

    fix for oblivion being all fucked

--- misc/games.nix
+++ misc/games.nix
@@ -6,24 +6,55 @@
   hardware.xpadneo.enable = true;
   hardware.steam-hardware.enable = true;
 
+  # Handle mouse polling not matching reported speeds:
+  boot.extraModprobeConfig = ''
+    options usbhid mousepoll=8
+  '';
+  # for debugging mouse shit:
+  # environment.systemPackages = with pkgs; [
+  #   pkgs.evhz
+  #   pkgs.usbutils
+  #   pkgs.libinput
+  #   pkgs.sysfsutils
+  # ];
+
+  programs.gamescope = {
+    enable = true;
+    env = {
+      # this was the key to getting oblivion to not be shit:
+      # <https://github.com/ValveSoftware/gamescope/issues/1592>
+      VKD3D_DISABLE_EXTENSIONS="VK_KHR_present_wait";
+    };
+  };
+
   # Supporting Packages
   environment.systemPackages = with pkgs; [
-      pkgs.gamescope
-      pkgs.gamemode
+    pkgs.gamemode
+    pkgs.prismlauncher
   ];
 
   programs.steam = {
     enable = true;
     gamescopeSession = {
       enable = true;
-      args = ["--hdr-enabled"];
+      env = {
+        # this was the key to getting oblivion to not be shit:
+        # <https://github.com/ValveSoftware/gamescope/issues/1592>
+        VKD3D_DISABLE_EXTENSIONS="VK_KHR_present_wait";
+      };
+      args = [
+        "-W 2560"
+        "-H 1440"
+        "-O DP-5"
+        "-r 60"
+        "-F nis"
+        "--expose-wayland"
+        "--force-grab-cursor"
+        "--rt"
+      ];
     };
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
-
-  users.users.ej.packages = with pkgs; [
-    prismlauncher
-  ];
 }
