b02598ab2d90058c70a24d481521ae92f6b9ae12
EntranceJew <EntranceJew@gmail.com>
2025-10-25 20:52:30 -0500

    attempt to make monitors work (they will not)

--- home-manager/misc/sway.nix
+++ home-manager/misc/sway.nix
@@ -1,3 +1,4 @@
+
 {
   config,
   pkgs,
@@ -7,8 +8,26 @@
   wayland.windowManager.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
-
     config = {
+      modifier = "Mod4";
+      output = {
+        "FWG43P3" = {
+          pos = "2560 0";
+          res = "2560x1440";
+        };
+        "8VL24P3" = {
+          pos = "2560 1440";
+          res = "2560x1440";
+        };
+        "714J7P3" = {
+          pos = "0 0";
+          res = "2560x1440";
+        };
+        "CGF55P3" = {
+          pos = "0 1440";
+          res = "2560x1440";
+        };
+      };
       /*
       your config go here
       */
