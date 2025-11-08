4df14ea2fb0d9a8630d7938424a2c25136c93734
EntranceJew <EntranceJew@gmail.com>
2025-11-08 15:05:42 -0600

    do it deploy

--- home-manager/programs/direnv.nix
+++ home-manager/programs/direnv.nix
@@ -1,6 +1,8 @@
 {
+  programs.bash.enable = true;
   programs.direnv = {
     enable = true;
+    enableBashIntegration = true;
     nix-direnv.enable = true;
     stdlib = ''
       : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
